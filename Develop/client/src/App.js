d2b55b6775690943ec3e51511b7858716dbcc355
4FunkE <4FunkEcodes@gmail.com>
2023-09-27 22:52:14 -0400

    removed the browser router in plave of apollo

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -1,30 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/client';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
 // Apollo Client instance
 const client = new ApolloClient({
-  uri: '/graphql', // Replace with your GraphQL server URI
+  uri: '/graphql',
   cache: new InMemoryCache(),
 });
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <>
-          <Navbar />
-          <Switch>
-            <Route exact path='/' component={SearchBooks} />
-            <Route exact path='/saved' component={SavedBooks} />
-            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-          </Switch>
-        </>
-      </Router>
+      <>
+        <Navbar />
+        <SearchBooks />
+        <SavedBooks />
+      </>
     </ApolloProvider>
   );
 }
